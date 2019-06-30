ad7d3ef58ca6ad240e5339bde4003ec1be4aded1
6Himanshu9 <42066317+6Himanshu9@users.noreply.github.com>
2019-06-30 16:24:38 +0530

    Add files via upload

---
+++ public/js/script.js
@@ -0,0 +1,102 @@
+'use strict';
+
+const socket = io();
+
+const outputYou = document.querySelector('.output-you');
+const outputBot = document.querySelector('.output-bot');
+
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+
+recognition.lang = 'en-US';
+recognition.interimResults = false;
+recognition.maxAlternatives = 1;
+
+document.querySelector('button').addEventListener('click', () => {
+  recognition.start();
+});
+
+recognition.addEventListener('speechstart', () => {
+  console.log('Speech has been detected.');
+});
+
+recognition.addEventListener('result', (e) => {
+  console.log('Result has been detected.');
+
+  let last = e.results.length - 1;
+  let text = e.results[last][0].transcript;
+
+  outputYou.textContent = text;
+  console.log('Confidence: ' + e.results[0][0].confidence);
+
+  socket.emit('chat message', text);
+});
+
+recognition.addEventListener('speechend', () => {
+  recognition.stop();
+});
+
+recognition.addEventListener('error', (e) => {
+  outputBot.textContent = 'Error: ' + e.error;
+});
+
+function synthVoice(text) {
+  const synth = window.speechSynthesis;
+  const utterance = new SpeechSynthesisUtterance();
+  utterance.text = text;
+  var voices = window.speechSynthesis.getVoices()
+  utterance.voice = voices[2]
+  utterance.lang='hi-IN'; 
+  utterance.volume=1;
+  utterance.rate=0.8; 
+  synth.speak(utterance);
+}
+
+// function loadVoices() {
+//   // Fetch the available voices.
+//   var voices = speechSynthesis.getVoices();
+  
+//   // Loop through each of the voices.
+//   voices.forEach(function(voice, i) {
+//     // Create a new option element.
+//     var option = document.createElement('option');
+    
+//     // Set the options value and text.
+//     option.value = voice.name;
+//     option.innerHTML = voice.name;
+      
+//     // Add the option to the voice selector.
+//     voiceSelect.appendChild(option);
+//   });
+// }
+
+// function synthVoice(text){
+//  // Execute loadVoices.
+// loadVoices();
+
+// // Chrome loads voices asynchronously.
+// window.speechSynthesis.onvoiceschanged = function(e) {
+//   loadVoices();
+// };
+
+// var utterance= new SpeechSynthesisUtterance();
+// utterance.text =text;
+// utterance.voice = speechSynthesis.getVoices().filter(function(voice) { return voice.name == "Google हिन्दी"; })[0];
+// window.speechSynthesis.speak(utterance);
+// }
+
+
+socket.on('bot reply', function(replyText) {
+ 
+
+  if(replyText == '') replyText = '(No answer...)';
+  outputBot.innerHTML = replyText;
+});
+
+//Bot Speech Reply
+socket.on('reply Speech', function(replySpeech) {
+ synthVoice(replySpeech);
+  
+});
+
+ 
---
+++ send/index.js
@@ -0,0 +1,74 @@
+'use strict';
+
+require('dotenv').config()
+const APIAI_TOKEN = process.env.APIAI_TOKEN;
+const APIAI_SESSION_ID = process.env.APIAI_SESSION_ID;
+
+const express = require('express');
+const app = express();
+
+
+
+app.use(express.static(__dirname + '/views')); // html
+app.use(express.static(__dirname + '/public')); // js, css, images
+
+const server = app.listen(process.env.PORT || 5000, () => {
+  console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
+});
+
+// const server = app.listen(3000, function(){
+// console.log('listening on  port %d', server.address().port);
+// });
+
+
+const io = require('socket.io')(server);
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+const apiai = require('apiai')(APIAI_TOKEN);
+
+// Web UI
+app.get('/', (req, res) => {
+  res.sendFile('index.html');
+});
+
+io.on('connection', function(socket) {
+  socket.on('chat message', (text) => {
+    console.log('Message: ' + text);
+
+    // Get a reply from API.ai
+
+    let apiaiReq = apiai.textRequest(text, {
+      sessionId: '<unique session id>'
+    });
+
+    // apiaiReq.on('response', (response) => {
+    //   let aiText = response.result.fulfillment.speech;
+    //   console.log('Bot reply: ' + aiText);
+    //   socket.emit('bot reply', aiText);
+    // });
+    var reply="",replySpeech="";
+    apiaiReq.on('response', (response) => {
+    let aiMessages = response.result.fulfillment.messages;
+    aiMessages.forEach(function (aiMessage){
+    reply=reply+aiMessage.speech+ "<br>";
+    replySpeech=replySpeech+aiMessage.speech+"! . ! ";
+      // console.log('Bot Reply: '+ aiMessage.speech);
+      // socket.emit('bot reply', aiMessage.speech);
+      });
+    
+      console.log('Bot Reply: '+ reply);
+      socket.emit('bot reply', reply);
+      socket.emit('replySpeech', replySpeech);
+
+      });
+    
+    apiaiReq.on('error', (error) => {
+      console.log(error);
+    });
+
+    apiaiReq.end();
+
+  });
+});
